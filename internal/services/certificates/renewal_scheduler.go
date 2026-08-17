931cd069d7fe2069df0d2bb475bb79002a164368
Snehal Dangroshiya <snehaldangroshiya@gmail.com>
2026-08-18 03:04:47 +0530

    refactor(certificates): move renewal scheduler onto schedulerkit.Runner

--- internal/services/certificates/renewal_scheduler.go
+++ internal/services/certificates/renewal_scheduler.go
@@ -5,13 +5,12 @@ import (
 	"time"
 
 	"rocketvault/internal/logging"
+	"rocketvault/internal/schedulerkit"
 )
 
 // CertificateRenewalScheduler runs CertificateRenewalService on a configurable interval.
 type CertificateRenewalScheduler struct {
-	svc      CertificateRenewalService
-	log      *logging.Logger
-	done     chan struct{}
+	runner   *schedulerkit.Runner
 	interval time.Duration
 }
 
@@ -21,45 +20,29 @@ func NewCertificateRenewalScheduler(svc CertificateRenewalService, log *logging.
 	if interval <= 0 {
 		interval = 24 * time.Hour
 	}
-	return &CertificateRenewalScheduler{svc: svc, log: log, done: make(chan struct{}), interval: interval}
+	check := func(ctx context.Context) error {
+		renewed, warned, err := svc.CheckAndRenewCertificates(ctx)
+		if err != nil {
+			log.WithError(err).Error("certificate renewal check failed")
+			return nil // handled here; nothing further for the Runner to log
+		}
+		if renewed > 0 || warned > 0 {
+			log.Infof("certificate renewal check: %d renewed, %d warned", renewed, warned)
+		}
+		return nil
+	}
+	return &CertificateRenewalScheduler{
+		runner:   schedulerkit.NewRunner("certificate renewal", check, log),
+		interval: interval,
+	}
 }
 
 // Start launches the scheduler in a background goroutine.
 func (s *CertificateRenewalScheduler) Start(ctx context.Context) {
-	go s.run(ctx)
+	_ = s.runner.Start(ctx, s.interval)
 }
 
 // Stop signals the scheduler to stop.
 func (s *CertificateRenewalScheduler) Stop() {
-	close(s.done)
-}
-
-func (s *CertificateRenewalScheduler) run(ctx context.Context) {
-	ticker := time.NewTicker(s.interval)
-	defer ticker.Stop()
-
-	// Run once immediately on startup.
-	s.check(ctx)
-
-	for {
-		select {
-		case <-ticker.C:
-			s.check(ctx)
-		case <-s.done:
-			return
-		case <-ctx.Done():
-			return
-		}
-	}
-}
-
-func (s *CertificateRenewalScheduler) check(ctx context.Context) {
-	renewed, warned, err := s.svc.CheckAndRenewCertificates(ctx)
-	if err != nil {
-		s.log.WithError(err).Error("certificate renewal check failed")
-		return
-	}
-	if renewed > 0 || warned > 0 {
-		s.log.Infof("certificate renewal check: %d renewed, %d warned", renewed, warned)
-	}
+	_ = s.runner.Stop()
 }
